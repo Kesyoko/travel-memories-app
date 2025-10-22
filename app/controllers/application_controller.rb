59492785901c1671652a87ea84746c2e27bdad95
杉本優希 <sugimotoyuuki@sugimotoyuukinoMacBook-Air.local>
2025-10-22 14:01:30 +0900

    ログイン機能・ログアウト機能を実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   allow_browser versions: :modern
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password ])
   end
 end
