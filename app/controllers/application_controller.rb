80e8aec76b99e9846279304c20e808d48eb63bae
杉本優希 <sugimotoyuuki@sugimotoyuukinoMacBook-Air.local>
2025-10-22 14:22:38 +0900

    各種チェックを忘れていたので再度プルリク

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index ]
+  # もしかしたらonly: [:index]?
   allow_browser versions: :modern
 
   def configure_permitted_parameters
