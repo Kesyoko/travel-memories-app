13e20dde3fbb7f78344b1b03eb05bb9ca1dfca6c
杉本優希 <sugimotoyuuki@sugimotoyuukinoMacBook-Air.local>
2025-12-13 05:11:05 +0900

    OGTが表示されなくなったのを修正しました

---
+++ db/migrate/20251212200553_fix_transportation_default_and_null.rb
@@ -0,0 +1,23 @@
+class FixTransportationDefaultAndNull < ActiveRecord::Migration[7.1]
+  def up
+    change_column_default :travel_records, :transportation, 9
+
+    
+    execute <<~SQL
+      UPDATE travel_records
+      SET transportation = 9
+      WHERE transportation IS NULL;
+    SQL
+  end
+
+  def down
+    # 元に戻したくなったときに使う
+    execute <<~SQL
+      UPDATE travel_records
+      SET transportation = NULL
+      WHERE transportation = 9;
+    SQL
+
+    change_column_default :travel_records, :transportation, nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_11_062115) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_12_200553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,7 +120,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_11_062115) do
     t.datetime "updated_at", null: false
     t.text "place_name"
     t.string "address"
-    t.integer "transportation"
+    t.integer "transportation", default: 9
     t.index ["travel_date"], name: "index_travel_records_on_travel_date"
     t.index ["user_id", "travel_date"], name: "index_travel_records_on_user_id_and_travel_date"
     t.index ["user_id", "want_to_visit_again"], name: "index_travel_records_on_user_id_and_want_to_visit_again"
