bbdb0b677d6b0903e5eac36579f2c94b7783a787
杉本優希 <sugimotoyuuki@sugimotoyuukinoMacBook-Air.local>
2026-01-14 01:31:05 +0900

    記録保存時にローディングアニメーションが出るようにしました

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,30 +1,38 @@
-// app/javascript/controllers/autocomplete_controller.js
+// stimulusというコントローラを呼ぶ
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="autocomplete"
+// valuesは外から渡される設定
 export default class extends Controller {
+  // HTMLから data-autocomplete-url-value を受け取る
   static values = { url: String }
+  // HTMLから data-autocomplete-target="results" を取得
   static targets = ["results"]
 
   search(event) {
-    console.log("autocomplete OK")
+    // target.valueで入力欄に入っている文字を取得、encodeURIComponentでエンコードしURLに使えるようにする
     const query = encodeURIComponent(event.target.value);  
+    // 上で変形したものを使ってURLを作成、thisはこのコントローラ自身を指す
     const url = `${this.urlValue}?q=${query}`;
 
+  // fetchでHTTPリクエストを出し、候補データを取得
     fetch(url)
       .then(response => response.json())
       .then(data => {
+        // resultsTarget(ul)を更新して候補リストを表示
         this.updateResults(data);
       })
       .catch(error => console.error('Error fetching autocomplete data:', error));
   }
 
   updateResults(data) {
+    // 一度候補を空白にする
     this.resultsTarget.innerHTML = '';
 
+    // liにして表示
     data.forEach(item => {
       const li = document.createElement('li');
       li.textContent = item.title;  
+      //　クリックされた候補を入力欄へ入れる
       li.addEventListener('click', () => {
         this.selectResult(item);
       });
@@ -33,9 +41,9 @@ export default class extends Controller {
   }
 
   selectResult(item) {
-    
-    this.element.querySelector('input').value = item.title;  
-
+    // 入力欄へ入れるための処理（タイトルをインプット欄へ）
+    this.element.querySelector('input').value = item.title;
+    // 候補を空白にする
     this.resultsTarget.innerHTML = '';
   }
 }
