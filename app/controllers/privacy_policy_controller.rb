c313d08bec5ffaa4dd6f210c723c4df78bfef7c8
杉本優希 <sugimotoyuuki@sugimotoyuukinoMacBook-Air.local>
2026-01-17 11:00:10 +0900

    グーグルログインデプロイ修正

--- app/controllers/privacy_policy_controller.rb
+++ app/controllers/privacy_policy_controller.rb
@@ -1,2 +1,6 @@
 class PrivacyPolicyController < ApplicationController
+  skip_before_action :authenticate_user!, raise: false
+
+  def show
+  end
 end
--- app/controllers/terms_of_use_controller.rb
+++ app/controllers/terms_of_use_controller.rb
@@ -1,2 +1,6 @@
 class TermsOfUseController < ApplicationController
+  skip_before_action :authenticate_user!, raise: false
+
+  def show
+  end
 end
